3a2870ef14cedce9af7de0f4f1aad34d5fddb8cf
Rodrigo Zamora <zamorarodrigo96@gmail.com>
2025-04-23 10:06:46 -0300

    Estilos del form  y carga dinamica de las monedas

---
+++ src/data/index.ts
@@ -0,0 +1,12 @@
+import { Currency } from "../types";
+
+export const currencies: Currency[] = [
+    { code: 'USD', name: 'Dolar de Estados Unidos' },
+    { code: 'MXN', name: 'Peso Mexicano' },
+    { code: 'EUR', name: 'Euro' },
+    { code: 'GBP', name: 'Libra Esterlina' },
+    { code: 'UYU', name: 'Peso Uruguayo' },
+    { code: 'ARS', name: 'Peso Argentino' },
+    { code: 'BRL', name: 'Real Brasileño' },
+    { code: 'CLP', name: 'Peso Chileno' }
+]
