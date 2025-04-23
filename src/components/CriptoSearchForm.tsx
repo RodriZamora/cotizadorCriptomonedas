7a1f87d87bf34071226c5ff7598ea66d31aa9e96
Rodrigo Zamora <zamorarodrigo96@gmail.com>
2025-04-23 11:43:28 -0300

    cargando las cryptos dinamicamente

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -1,6 +1,10 @@
+import { useCryptoStore } from '../store'
 import { currencies } from '../data'
 
 export default function CriptoSearchForm() {
+
+    const cryptocurrencies = useCryptoStore((state) => state.cryptocurrencies);
+
     return (
         <form className='form'>
             <div className='field'>
@@ -17,7 +21,9 @@ export default function CriptoSearchForm() {
                 <label htmlFor="currency">Criptomoneda: </label>
                 <select name="criptocurrency" id="criptocurrency">
                     <option value="">-- Seleccione --</option>
-
+                    {cryptocurrencies.map(crypto => (
+                        <option key={crypto.CoinInfo.FullName} value={crypto.CoinInfo.Name}>{crypto.CoinInfo.FullName}</option>
+                    ))}
                 </select>
             </div>
 
